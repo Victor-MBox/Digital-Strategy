0d084b5bbf553477ce2b5d8becaa976a23aa89e6
Victor Matsakov <victor.matsakov7@gmail.com>
2024-04-24 22:09:16 +0700

    finish

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
 'use strict'
+
+function initSwiper() {
+	if (window.swiperInstance) {
+		window.swiperInstance.destroy(true, true)
+		window.swiperInstance = null
+	}
+
+	// Получаем ширину окна
+	const screenWidth =
+		window.innerWidth ||
+		document.documentElement.clientWidth ||
+		document.body.clientWidth
+
+	// Инициализируем слайдер, только если ширина экрана меньше 767 пикселей
+	if (screenWidth < 767) {
+		window.swiperInstance = new Swiper('.swiper', {
+			loop: true,
+			slidesPerView: 1.2,
+			/* breakpoints: {
+				480: {
+					slidesPerView: 2,
+				},
+				320: {
+					slidesPerView: 1.2,
+				},
+			}, */
+		})
+	} 
+}
+
+// Инициализируем слайдер при загрузке содержимого страницы
+document.addEventListener('DOMContentLoaded', initSwiper)
+
+// Переинициализируем слайдер при изменении размеров окна
+window.addEventListener('resize', initSwiper)
